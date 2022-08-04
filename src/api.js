ed4ffbff9b9d888799cb1cef1e589be831b9b0cd
wintaye <41543902+wintaye@users.noreply.github.com>
2022-08-03 23:33:21 -0500

    implemented first cities API

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5a5ba09dfamshaddfe41d4399abcp14fc4djsnd8d32d6f9592',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
